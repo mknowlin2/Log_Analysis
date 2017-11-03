19ac6a1173fd4d49ef6e91a1ac844d9bba4a3ea4
Michael A. Knowlin II <mknowlin2@gmail.com>
2017-11-02 23:25:48 -0400

    Created LOG_TMPLT2 for displaying get_request_error_log results.

--- src/news.py
+++ src/news.py
@@ -13,6 +13,10 @@ LOG_TMPLT = '''\
     <div class=logs>{} — {} views</div>
 '''
 
+LOG_TMPLT2= '''\
+    <div class=logs>{} — {}% errors</div>
+'''
+
 
 @app.route('/', methods=['GET'])
 def main():
@@ -29,7 +33,7 @@ def main():
            for title, view_cnt in get_top_three_articles())
     logs = logs + "".join(LOG_TMPLT.format(title, view_cnt) \
            for title, view_cnt in get_most_pop_author())
-    logs = logs + "".join(LOG_TMPLT.format(title, view_cnt) \
+    logs = logs + "".join(LOG_TMPLT2.format(title, view_cnt) \
            for title, view_cnt in get_request_error_log())
 
     html = data % logs
