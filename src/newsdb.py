8b11149008862d5ed9006dc346cc3562c9f29d6b
Michael A. Knowlin II <mknowlin2@gmail.com>
2017-11-05 12:40:24 -0500

    Added the connect() method to cut down on code duplication.

--- src/newsdb.py
+++ src/newsdb.py
@@ -6,38 +6,45 @@ import psycopg2
 DBNAME = "news"
 
 
+def connect(database_name):
+    """Obtain database connection and cursor"""
+    try:
+        db = psycopg2.connect(database=database_name)
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("Failed to connect to {}".format(database_name))
+
+
 def get_top_three_articles():
     """Return most popular articles of all times in descending order"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
+    db, cursor = connect(DBNAME)
 
-    c.execute("select title, views from article_views \
+    cursor.execute("select title, views from article_views \
                order by views desc limit 3;")
 
-    return c.fetchall()
+    return cursor.fetchall()
     db.close()
 
 
 def get_most_pop_author():
     """Return most popular author of all times in descending order"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
+    db, cursor = connect(DBNAME)
 
-    c.execute("select t1.name, sum(t2.views) as views \
+    cursor.execute("select t1.name, sum(t2.views) as views \
                from authors t1, article_views t2 \
                where t1.id = t2.author group by t1.name order by views desc")
 
-    return c.fetchall()
+    return cursor.fetchall()
     db.close()
 
 
 def get_request_error_log():
     """Return a list of all days where more than '1%' of requests lead to
        errors"""
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
+    db, cursor = connect(DBNAME)
 
-    c.execute("select log_date, to_char(err_pct, '99.9') \
+    cursor.execute("select log_date, to_char(err_pct, '99.9') \
                from (select t1.log_date, t2.status_cnt * 100::numeric \
                      / (t1.status_cnt + t2.status_cnt) as err_pct \
                      from request_stat t1, request_stat t2 \
@@ -46,5 +53,5 @@ def get_request_error_log():
                      and t1.status != t2.status) as temp1 \
                where err_pct > 1")
 
-    return c.fetchall()
+    return cursor.fetchall()
     db.close()
